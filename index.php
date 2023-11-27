f3e5a153997ea647b367b6331538d26d34052b6c
Ayush Pakhrin <pakhrinayush56@gmail.com>
2023-11-27 17:55:04 +0545

    modified navbar added categories

--- index.php
+++ index.php
@@ -37,9 +37,29 @@ include ('./includes/connect.php');
         <li class="nav-item">
           <a class="nav-link" href="#">Products</a>
         </li>
+        <!-- dropdown -->
         <li class="nav-item">
-          <a class="nav-link" href="#">Register</a>
-        </li>
+        <div class="dropdown show">
+        <a class="btn dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        categories
+        </a>
+        <!-- items to display in meny -->
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+        <?php
+            $select_category="select * from `categories`";
+            $result_category=mysqli_query($con,$select_category);
+            while($row_data=mysqli_fetch_assoc($result_category)){
+              $category_title=$row_data['category_title'];
+              $category_id=$row_data['category_id'];
+              echo "
+              <a href='index.php?categories=$category_id' class='dropdown-item text-capitalize'>$category_title</a>
+              ";
+            }
+          ?>
+          </div>
+        </div>
+</li>
+        <!-- dropdown end -->
         <li class="nav-item">
           <a class="nav-link" href="#">Contact</a>
         </li>
@@ -150,7 +170,8 @@ include ('./includes/connect.php');
     </div>
     
 <!-- bootstrap js -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
