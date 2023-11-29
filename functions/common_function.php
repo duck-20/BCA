14a30fbae70e00861ae7128099b7656ab1918df4
Ayush Pakhrin <pakhrinayush56@gmail.com>
2023-11-29 19:30:04 +0545

    added unique categorires

--- functions/common_function.php
+++ functions/common_function.php
@@ -21,7 +21,10 @@ function getCategories(){
 
 function getProducts(){
     global $con;
-    $select_query="select * from `products` order by rand()";
+    //condition to check isset or not:
+            if(!isset($_GET['categories'])){
+
+            $select_query="select * from `products` order by rand()";
             $result_query=mysqli_query($con,$select_query);
             while($row=mysqli_fetch_assoc($result_query)){
                 $product_id=$row['product_id'];
@@ -34,18 +37,53 @@ function getProducts(){
                 echo " <div class='col-md-4 mb-3'>
                 
                 <div class='card'>
-  <img src='./admin_area/product_images/$product_image1' class='card-img-top p-2' alt='...'>
-  <div class='card-body'>
-    <h5 class='card-title'>$product_name</h5>
-    <p class='card-text'>$product_description</p>
-      <a href='#' class='btn btn-primary'>Add To Cart</a>
-                    <a href='#' class='btn btn-secondary'>View More</a>
-                    
-            </div>
-          </div>
-        </div>";
-            }
+                    <img src='./admin_area/product_images/$product_image1' class='card-img-top p-2' alt='...'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>$product_name</h5>
+                        <p class='card-text'>$product_description</p>
+                        <a href='#' class='btn btn-primary'>Add To Cart</a>
+                                        <a href='#' class='btn btn-secondary'>View More</a>
+                                        
+                                </div>
+                            </div>
+                            </div>";
+                }
+        }
 }
-
-
+//getting a single categories
+function getUniqueCategories(){
+    global $con;
+    //condition to check isset or not:
+            if(isset($_GET['categories'])){
+            $category_id=$_GET['categories'];
+            $select_query="select * from `products` where category_id=$category_id";
+            $result_query=mysqli_query($con,$select_query);
+            $num_of_rows=mysqli_num_rows($result_query);
+            if($num_of_rows===0){
+                echo " <h2 class='text-center text-danger'>No Stock For This Category</h2>";
+            }
+            while($row=mysqli_fetch_assoc($result_query)){
+                $product_id=$row['product_id'];
+                $product_name=$row['product_name'];
+                $product_description=$row['product_description'];
+                $product_keywords=$row['product_keywords'];
+                $product_image1=$row['product_image1'];
+                $product_price=$row['product_price'];
+                $category_id=$row['category_id'];
+                echo " <div class='col-md-4 mb-3'>
+                
+                <div class='card'>
+                    <img src='./admin_area/product_images/$product_image1' class='card-img-top p-2' alt='$product_name'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>$product_name</h5>
+                        <p class='card-text'>$product_description</p>
+                        <a href='#' class='btn btn-primary'>Add To Cart</a>
+                                        <a href='#' class='btn btn-secondary'>View More</a>
+                                        
+                                </div>
+                            </div>
+                            </div>";
+                }
+        }
+}
 ?>
